b2e4c341fb5cdfb39dbe97033c06ac3a1c0b1a9a
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-16 22:04:42 +0100

    create get_sales_data function to get sales data

--- run.py
+++ run.py
@@ -14,7 +14,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS) #To authorize the scope
 SHEET = GSPREAD_CLIENT.open('love_sandwiches') #Using open method on Gsheet file 'love-sandwiches
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
+# sales = SHEET.worksheet('sales')
+# data = sales.get_all_values()
 
-print(data)
+# print(data)
+
+def get_sales_data():
+    """
+    Get sales figures inpurt from user 
+    """
+    print("Enter sales data from the last market.,\nData should be six numbers, separated by commaes,\nExample: 10,20,3,4,5,5")
+
+    data_str = input("Enter your data here: ")
+    
+    print(f"\nThe data you provided is {data_str} ")
+
+get_sales_data()        
