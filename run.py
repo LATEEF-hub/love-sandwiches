b15027322b25732faf99456dd24d92954efdf567
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-17 20:32:31 +0100

    add validation to check for 6 values in input Data

--- run.py
+++ run.py
@@ -7,17 +7,19 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
-CREDS = Credentials.from_service_account_file('creds.json')  
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)  
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS) #To authorize the scope
-SHEET = GSPREAD_CLIENT.open('love_sandwiches') #Using open method on Gsheet file 'love-sandwiches
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)  # To authorize the scope
+# Using open method on Gsheet file 'love-sandwiches
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 # sales = SHEET.worksheet('sales')
 # data = sales.get_all_values()
 
 # print(data)
+
 
 def get_sales_data():
     """
@@ -26,7 +28,23 @@ def get_sales_data():
     print("Enter sales data from the last market.,\nData should be six numbers, separated by commaes,\nExample: 10,20,3,4,5,5")
 
     data_str = input("Enter your data here: ")
-    
-    print(f"\nThe data you provided is {data_str} ")
+    # print(f"\nThe data you provided is {data_str} ")
+    sales_data = data_str.split(",")
+    # print(sales_data)
+    validate_data(sales_data)
 
-get_sales_data()        
+def validate_data(values):
+        """
+        Use Try and Exception method to validate, Inside Try convert all Str to Int
+        Raise errors if less than 6 or greater and if Str cannot be converted.
+        """
+        try:
+            if len(values) != 6:
+                raise ValueError(
+                    f'Enter 6 figures exactly, you provided {len(values)} '
+                )
+        except ValueError as e:
+            print(f"Invalid data: {e}, Please try again.\n")
+         
+        
+get_sales_data() #We have to call the function for the CODE it to run...6.
